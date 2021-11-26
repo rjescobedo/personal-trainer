8ce02df05ec166cfb4a641e8a53991b3085983f1
Raul Escobedo <rjescobedo@gmail.com>
2021-11-26 09:23:47 -0600

    Login jQuery

--- js/script.js
+++ js/script.js
@@ -1,64 +1,54 @@
 $(function(){
-//Login Modal
-    $("#loginButton").click(function(){
-        $("#loginModal").modal("show");
-
-    });
-
-//Logout to index.html
-$('#logoutButton').click(function(){
-    window.location.href = 'index.html';
-    return false;
-})
-
-//If Statement for Login
-/* $('#signInToPage').click(function() {
-    if ($('#clientSelection').click){
-        window.location.href = 'client.html';
+    //Login Modal
+        $("#loginButton").click(function(){
+            $("#loginModal").modal("show");
+    
+        });
+    
+    //Logout to index.html
+    $('#logoutButton').click(function(){
+        window.location.href = 'index.html';
         return false;
-    } else {
-        window.location.href = 'trainer.html';
-        return false;
-    }
-}); */
-
-//Signin to Client Page
-    $('#clientSelection').click(function(){
-        window.location.href = 'client.html';
-        return false;
-    });
-
-//Signin to Trainer Page
-    $('#trainerSelection').click(function(){
-        window.location.href = 'trainer.html';
-        return false;
-    });
-
-//Chest Press Modal
-    $('#chestPressButton').click(function(){
-        $('#chestPressModal').modal('show');
-    });
-
-//Congrats Modal
-    $('#upperBodyComplete').click(function(){
-        $('#congratsModal').modal('show');
+    })
+    
+    $("#loginForm").submit(function(e){
+        e.preventDefault();
+        const form = this;
+        if ( $('input[name="customRadioInline1"]:checked').val() === "client"){
+            this.action = "client.html";
+            form.submit();
+        } else {
+            this.action = "trainer.html";
+            form.submit();
+        }
+    })
+    
+    //Chest Press Modal
+        $('#chestPressButton').click(function(){
+            $('#chestPressModal').modal('show');
+        });
+    
+    //Congrats Modal
+        $('#upperBodyComplete').click(function(){
+            $('#congratsModal').modal('show');
+        });
+        
+        $('#lowerBodyComplete').click(function(){
+            $('#congratsModal').modal('show');
+        });
+    
+    //Contact Modal
+        $('#submitContact').click(function(){
+            $('#contactModal').modal('show');
+        });
+    
+    //Footer Tooltips
+        $('[data-toggle="tooltip"]').tooltip();
+    
+    //Trainer Workout Modal
+        $('#workoutSubmitButton').click(function(){
+            $('#workoutSubmitModal').modal('show');
     });
     
-    $('#lowerBodyComplete').click(function(){
-        $('#congratsModal').modal('show');
     });
-
-//Contact Modal
-    $('#submitContact').click(function(){
-        $('#contactModal').modal('show');
-    });
-
-//Footer Tooltips
-    $('[data-toggle="tooltip"]').tooltip();
-
-//Trainer Workout Modal
-    $('#workoutSubmitButton').click(function(){
-        $('#workoutSubmitModal').modal('show');
-});
-
-});
+    
